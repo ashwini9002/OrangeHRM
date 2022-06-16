b59ae285e43a808167e49099bb01a76984113fbe
Ashwini Shewale <ashewale@delaplex.com>
2022-06-16 18:25:17 +0530

    new method added in homepage class

--- Pages/Homepage.py
+++ Pages/Homepage.py
@@ -11,6 +11,9 @@ class Homepage(BaseClass):
         self.driver= driver
 
     tags = (By.XPATH,"//div[@class='post-info']/p[@class='tags']/a")
+    user= (By.XPATH,"//input[@name='txtUsername']")
+    pwd = (By.XPATH,"//input[@name='txtPassword']")
+    login_button= (By.XPATH,"//input[@name='Submit']")
 
     def click_tags_one_by_one(self):
         tag_elements= self.driver.find_elements(*Homepage.tags)
@@ -19,3 +22,8 @@ class Homepage(BaseClass):
         for tag in tag_elements:
             act.key_down(Keys.CONTROL).click(tag).perform()
             print(tag.text)
+
+    def login(self,uname,password):
+        self.driver.find_element(*Homepage.user).send_keys(uname)
+        self.driver.find_element(*Homepage.pwd).send_keys(password)
+        self.driver.find_element(*Homepage.login_button).click()
